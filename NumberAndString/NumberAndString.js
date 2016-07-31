1ef43d957482f74a6d3d7591a9b9ab2440abbea5
HyeongJu Yoo <zerofunc@naver.com>
2016-07-31 23:42:10 +0900

    Add NumberAndString

---
+++ NumberAndString/NumberAndString.js
@@ -0,0 +1,33 @@
+function printNumber() {
+    alert(1 + 1);
+    alert(1.2 + 2.3);
+    alert(2 * 5);
+    alert(6 / 2);
+}
+
+function printMathFunc() {
+    alert(Math.pow(3, 2));
+    alert(Math.round(10.6));
+    alert(Math.ceil(10.2));
+    alert(Math.floor(10.6));
+    alert(Math.sqrt(9));
+    alert(Math.random());
+}
+
+function printString() {
+    alert("coding");
+    alert('coding');
+
+    alert(typeof 1);
+
+    alert('egoing\'s javascript');
+    
+    alert('Hellow \nnew Line')
+}
+
+function printCalcOfString(){
+    alert("let's" + "coding");
+    alert('coding everbody'.length);
+}
+printCalcOfString();
+
