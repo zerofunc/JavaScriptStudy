4dc2cfb7e27294ab4998aab8388c8b393ecc2bbb
HyeongJu Yoo <zerofunc@naver.com>
2016-08-02 23:51:37 +0900

    Add ConditionStatement

---
+++ ConditionStatement/conditionStatement.js
@@ -0,0 +1,21 @@
+if(!'') {
+    alert('빈 문자열');
+}
+
+if(!undefined) {
+    alert('undefined');
+}
+
+var tmp;
+
+if(!tmp) {
+    alert('값이 할당되지 않은 변수');
+}
+
+if(!null) {
+    alert('null');
+}
+
+if(!NaN) {
+    alert('NaN');
+}
