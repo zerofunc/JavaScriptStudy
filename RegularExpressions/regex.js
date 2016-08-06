3f24ad88e481af5bafaebf1e5743ffaa56bcc883
HyeongJu Yoo <zerofunc@naver.com>
2016-08-06 23:52:04 +0900

    update regex.js
    add isKorea and isNumber regex function

--- RegularExpressions/regex.js
+++ RegularExpressions/regex.js
@@ -1,7 +1,20 @@
 var regex = {
+    isKorean : function(str) {
+        return regex.chk(/^[가-힣]+$/,str, "한글이 아닙니다.");
+    },
+    
+    isNumber : function(str) {
+        return regex.chk(/^[0-9]+$/,str, "숫자가 아닙니다.");
+    },
+    
+    isAlphabet : function(str) {
+        return regex.chk(/^[a-zA-Z]+$/,str, "영문이 아닙니다.");
+    },
+    
     isPhoneNumber: function (str) {
         return regex.chk(/^\d{3}-\d{3,4}-\d{4}$/,str, "폰번호 형식이 아닙니다.");
     },
+    
     chk: function (re, str, msg) {
         if(!re.test(str)) {
             alert(msg);
@@ -13,6 +26,6 @@ var regex = {
 };
 
 
-var data = "010-123-4567"
+var data = "안녕"
 
-console.log(regex.isPhoneNumber(data));
+console.log(regex.isKorean(data));
